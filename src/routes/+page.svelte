29d31d34397f8809ba8c251d08216ccb0ecdc164
Baraa <124203201+0baraa@users.noreply.github.com>
2025-06-26 11:53:14 +0100

    Add 1k most common english words and print in console

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>dolphintype</h1>
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const wordsResponse = await fetch('/english1k.json');
+		const allWords = await wordsResponse.json();
+
+		console.log(allWords);
+	});
+</script>
